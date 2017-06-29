d86679fbca5705a0199f3e6798c650935baf9540
martrinity <martiloca_4@hotmail.com>
2017-06-29 10:15:36 -0400

    ingresa tarea como checkbox

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,12 @@
 $(document).ready(function() {
+	var counter = 0;
 	$("#agregar").click(function(ev) {
 		ev.preventDefault();
 		var tareaNueva = $("#tarea").val();
 		if(tareaNueva ==""){
 			alert("Ingrese tareas en su lista"); 
 		}else{
-			$("#checkbox1").before("<input type='checkbox' name='tarea'>" + tareaNueva + "</input><button id='borrar'>Eliminar</button>");
+			$("#checkbox1").append("<input type='checkbox' name='tarea'><label>" + tareaNueva + "</label><button id='borrar' class='right'>Eliminar</button><br>");
 		}
-	})
+	});
 });
