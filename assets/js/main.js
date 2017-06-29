94a5d514c0928aa669910d8a536d6715284f59d1
martrinity <martiloca_4@hotmail.com>
2017-06-29 11:07:01 -0400

    modificando js para eliminar/cambniar a done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,19 @@
 $(document).ready(function() {
-	var counter = 0;
 	$("#agregar").click(function(ev) {
 		ev.preventDefault();
 		var tareaNueva = $("#tarea").val();
 		if(tareaNueva ==""){
 			alert("Ingrese tareas en su lista"); 
 		}else{
-			$("#checkbox1").append("<input type='checkbox' name='tarea'><label>" + tareaNueva + "</label><button id='borrar' class='right'>Eliminar</button><br>");
+			$("#checkbox1").append("<input id="" type='checkbox' name='tarea'><label>" + tareaNueva + "</label><button id='borrar' class='btn-small right'>Eliminar</button><br>");
 		}
+		
 	});
+	$("#checkbox1 :checkbox").change(function() {
+		$("#checkbox1").prop('checked', true);
+        $("#checkbox1").prop('checked', false);
+        if(this.checked){
+        	$("")
+        }
+    }
 });
