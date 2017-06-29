f71889d74345862921524b03bfe2caf7d580311e
martrinity <martiloca_4@hotmail.com>
2017-06-29 03:06:33 -0400

    js ingresa tarea

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,11 @@
-main.js
+$(document).ready(function() {
+	$("#agregar").click(function(ev) {
+		ev.preventDefault();
+		var tareaNueva = $("#tarea").val();
+		if(tareaNueva ==""){
+			alert("Ingrese tareas en su lista"); 
+		}else{
+			$("#checkbox1").before("<input type='checkbox' name='tarea'>" + tareaNueva + "</input><button id='borrar'>Eliminar</button>");
+		}
+	})
+});
